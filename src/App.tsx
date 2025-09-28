c89e16e007e5d8f7b16f608685ab2750613a7804
Niharika Chunchu <niharikachunchu10@gmail.com>
2025-09-28 13:35:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { Header } from "@app/components/Header"
-import { Stories } from "@app/containers/Stories"
-import { ContentMedia } from "@app/containers/ContentMedia"
-import { AppContext } from "@app/contexts"
-import { getInitialState } from "@app/services/getInitialState"
-import { useEffect, useReducer } from "react"
-import { reducerFn } from "@app/reducers"
-import { Portal } from "@app/portals"
-import { StoryPortal } from "@app/portals/StoryPortal"
+import { Header } from "@app/components/Header";
+import { Stories } from "@app/containers/Stories";
+import { ContentMedia } from "@app/containers/ContentMedia";
+import { AppContext } from "@app/contexts";
+import { getInitialState } from "@app/services/getInitialState";
+import { useReducer } from "react";
+import { reducerFn } from "@app/reducers";
+import { Portal } from "@app/portals";
+import { StoryPortal } from "@app/portals/StoryPortal";
+import { IAppContext, IPayload } from "@app/contexts/types.interface";
 
 function App() {
-  const [state, dispatch] = useReducer(reducerFn, getInitialState())
-  const globalState = {
+  const [state, dispatch] = useReducer<React.Reducer<IAppContext, IPayload>>(
+    reducerFn,
+    getInitialState()
+  );
+
+  const globalState: IAppContext = {
     ...state,
     dispatch
-  }
+  };
 
   return (
     <AppContext.Provider value={globalState}>
-      <div className="App h-auto min-h-screen w-full bg-black">
-        <section className="w-full max-w-[900px] mx-auto h-full">
+      <div className="App flex flex-col min-h-screen w-full bg-black">
+        {/* Main content */}
+        <section className="w-full max-w-[900px] mx-auto flex-1">
           <Header />
           <Stories />
           <ContentMedia />
         </section>
 
+        {/* Portals */}
         <Portal>
           <StoryPortal />
         </Portal>
       </div>
     </AppContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
